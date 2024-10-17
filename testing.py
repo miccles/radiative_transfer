cb0afe4bb5efd6f0cede54e08170cb943f198ab3
Michail Damoulakis <mdamoula@purdue.edu>
2024-10-17 08:37:14 -0400

    Update testing.py

--- testing.py
+++ testing.py
@@ -5,7 +5,7 @@ def func(x):
     return np.tanh(x)
 
 Nmin = 10 ** 2
-Nmax = 10 ** 8
+Nmax = 10 ** 5
 N_range = np.logspace(np.log10(Nmin), np.log10(Nmax), 1000, dtype=int)
 
 a = 0
@@ -20,10 +20,25 @@ int_values = [(b - a) * np.mean(g) for g in g_i]
 error = [np.abs(int_exact - int_val) for int_val in int_values]
 
 
-plt.plot(N_range, error)
-plt.plot(N_range, [error[0] / np.sqrt(N/N_range[0]) for N in N_range], '--')
-plt.xlabel('N')
-plt.ylabel('Absolute Error')
-plt.xscale('log')
-plt.yscale('log')
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+
+# Left subplot
+ax1.plot(N_range, int_values, label='int_values')
+ax1.hlines(int_exact, Nmin, Nmax, label='int_exact', linestyle='--', color='red')
+ax1.set_xlabel('N')
+ax1.set_ylabel('Values')
+ax1.set_xscale('log')
+ax1.set_yscale('log')
+ax1.legend()
+
+# Right subplot
+ax2.plot(N_range, error, label='error')
+ax2.plot(N_range, [error[0] / np.sqrt(N/N_range[0]) for N in N_range], '--', label='error trend')
+ax2.set_xlabel('N')
+ax2.set_ylabel('Absolute Error')
+ax2.set_xscale('log')
+ax2.set_yscale('log')
+ax2.legend()
+
 plt.show()
